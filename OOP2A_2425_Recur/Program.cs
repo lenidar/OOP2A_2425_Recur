d4c553388d3664219d4f206e850212113b05e89c
Julian Laspona <JulianDevAcc@hotmail.ph>
2024-09-25 14:28:24 +0800

    Add project files.

---
+++ OOP2A_2425_Recur/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2A_2425_Recur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(multiplication(2,2,0));
+            Console.WriteLine(multiplication(11, 12, 0));
+            Console.WriteLine(multiplication(11, 5, 0));
+            Console.WriteLine(multiplication(11, 5));
+            Console.ReadKey();
+        }
+
+        static int multiplication(int a, int b, int ans)
+        {
+            ans += a;
+            b--;
+            if(b <= 0)
+                return ans;
+            else
+                return multiplication(a,b,ans);
+        }
+
+        static int multiplication(int a, int b)
+        {
+            int ans = a;
+            b--;
+            if (b > 0)
+                ans += multiplication(a, b);
+
+            return ans;
+        }
+    }
+}
